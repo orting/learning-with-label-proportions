d5a3de65f25091e34f57218105d7f9fc7912020f
Silas Ørting <silas.orting@gmail.com>
2017-03-21 16:54:30 +0100

    Removed FileTracer from inheritance hierarchy and reimplemented tracer functions

--- include/llp/Tracers/FileTracer.h
+++ include/llp/Tracers/FileTracer.h
@@ -1,31 +1,74 @@
 #ifndef __FileTracer_h
 #define __FileTracer_h
 
-#include "SilentTracer.h"
-
-class FileTracer : public SilentTracer {
+class FileTracer {
 public:
-  struct ParameterType : public SilentTracer::ParameterType {
+  enum Level {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+  };
+
+  struct ParameterType  {
     ParameterType(Level level=Level::DEBUG, const std::string& path=std::string())
-      : SilentTracer::ParameterType(level)
+      : level(level)
       , path(path)
     {}
     
+    Level level;
     std::string path;
   };
     
   FileTracer(const ParameterType& params)
-    : SilentTracer(params)
+    : m_Level(params.level)
     , m_Out(params.path)
   {}
+
+  template<typename T>
+  void Trace(const std::string& s, const T& x) {
+    if (m_Level <= Level::TRACE) {
+      write("TRACE", s, x);
+    }
+  }
+
+  template<typename T>
+  void Debug(const std::string& s, const T& x) {
+    if (m_Level <= Level::DEBUG) {
+      write("DEBUG", s, x);
+    }
+  }
+  
+  template<typename T>
+  void Info(const std::string& s, const T& x) {
+    if (m_Level <= Level::INFO) {
+      write("INFO", s, x);
+    }
+  }
+
+  template<typename T>
+  void Warning(const std::string& s, const T& x) {
+    if (m_Level <= Level::WARNING) {
+      write("WARNING", s, x);
+    }
+  }
+  
+  template<typename T>
+  void Error(const std::string& s, const T&x) {
+    if (m_Level <= Level::ERROR) {
+      write("ERROR", s, x);
+    }
+  }
 
   
 protected:
   template<typename T>
   void write(const std::string& prefix, const std::string& msg, const T& x) {
-    m_Out << '[' << prefix << "] " << msg << " : " << x << '\n';
+    m_Out << '[' << prefix << "] " << msg << " : " << x << std::endl;
   }
   
+  Level m_Level;
   std::ofstream m_Out;
 };
 
